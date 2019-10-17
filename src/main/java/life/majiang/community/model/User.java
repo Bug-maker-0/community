56d8835307dd0b4d52f404a86eb68c969d1c58cf
Bug-maker-0 <39735864+Bug-maker-0@users.noreply.github.com>
2019-10-17 14:09:52 +0800

    add sql ddl

---
+++ src/main/java/life/majiang/community/model/User.java
@@ -0,0 +1,61 @@
+package life.majiang.community.model;
+
+/**
+ * Created by uncle on 2019/10/10.
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String accountId;
+    private String token;
+    private Long gmtCreate;
+    private Long gmtModified;
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public Long getGmtCreate() {
+        return gmtCreate;
+    }
+
+    public void setGmtCreate(Long gmtCreate) {
+        this.gmtCreate = gmtCreate;
+    }
+
+    public Long getGmtModified() {
+        return gmtModified;
+    }
+
+    public void setGmtModified(Long gmtModified) {
+        this.gmtModified = gmtModified;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
